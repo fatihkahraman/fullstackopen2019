d34ef14e2ac82ad5138f025e2802da173a53022d
Fatih Kahraman <fatih.hda@gmail.com>
2019-08-26 19:12:45 +0200

    unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,10 +9,35 @@ const Header = ({title}) => {
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
-const Content = ({text, counter}) => {
-    console.log(text, counter);
+const Content = ({text, counter}) => <p>{text} {counter}</p>
+
+const Positive = (props) => {
+    const good = props.counter[0]
+    const neutral = props.counter[1]
+    const bad = props.counter[2]
+    let pos = (good/(good+neutral+bad))*100
+
+    if(isNaN(pos)) {
+        pos = 0
+    }
+
     return (
-        <p>{text} {counter}</p>
+        <p>{props.text} {pos} %</p>
+    )
+}
+
+const Average = (props) => {
+    const good = props.counter[0]
+    const neutral = props.counter[1]
+    const bad = props.counter[2]
+    let avg = (good+(bad*(-1)))/(good+neutral+bad)
+
+    if(isNaN(avg)) {
+        avg = 0
+    }
+
+    return (
+        <p>{props.text} {avg}</p>
     )
 }
 
@@ -35,6 +60,9 @@ const App = () => {
             <Content text='good' counter={good}/>
             <Content text='neutral' counter={neutral}/>
             <Content text='bad' counter={bad}/>
+            <Content text='all' counter={good+neutral+bad}/>
+            <Average text='average' counter={[good,neutral,bad]}/>
+            <Positive text='positive' counter={[good,neutral,bad]}/>
         </>
     )
 }
